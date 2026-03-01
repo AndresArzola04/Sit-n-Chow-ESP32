c418e847c5a7f10201ca15849ef745957be4bb48
ManMadePancake <arzolaandres1@gmail.com>
2026-02-28 22:34:30 -0500

    First Commit

---
+++ main/camera_pin.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define CAMERA_PIN_PWDN   -1
+#define CAMERA_PIN_RESET  -1
+#define CAMERA_PIN_XCLK   4
+#define CAMERA_PIN_SIOD   5
+#define CAMERA_PIN_SIOC   6
+
+#define CAMERA_PIN_D7     7
+#define CAMERA_PIN_D6     15
+#define CAMERA_PIN_D5     16
+#define CAMERA_PIN_D4     17
+#define CAMERA_PIN_D3     18
+#define CAMERA_PIN_D2     8
+#define CAMERA_PIN_D1     3
+#define CAMERA_PIN_D0     46
+#define CAMERA_PIN_VSYNC  9
+#define CAMERA_PIN_HREF   10
+#define CAMERA_PIN_PCLK   11
